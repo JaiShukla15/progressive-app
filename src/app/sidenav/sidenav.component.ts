5f3094d515f25781435ce03dbd3c57da505c80c7
jai shukla <shuklajay012@gmail.com>
2023-07-23 03:12:00 +0530

    hotfix

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -21,6 +21,10 @@ export class SidenavComponent {
 
   @Input() isLogin:boolean = false;
   @HostListener('window:resize', ['$event'])
+
+  ngOnint(){
+    this.onResize();
+  }
   onResize() {
     this.innerWidth = window.innerWidth;
     this.isMobile = this.innerWidth < 800;
