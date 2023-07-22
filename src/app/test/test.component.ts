3cfa34ffb9999565138469c7272d7dd428aec840
jai shukla <shuklajay012@gmail.com>
2023-07-23 04:09:16 +0530

    hotfix

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,6 +9,8 @@ import { ApiService } from '../services/api.service';
 })
 export class TestComponent {
 
+  public backgroundFetchUrl:string = 'https://onlinetestcase.com/wp-content/uploads/2023/06/1.5-MB.pdf';
+
   constructor(
     private api:ApiService
   ){}
@@ -37,5 +39,35 @@ export class TestComponent {
         }]
      })
     })
+  }
+  async performBackgroundFetch() {
+    try {
+      const registration = await navigator.serviceWorker.ready;
+      const controller = registration.active;
+
+      const backgroundFetch = await (self as any).backgroundFetch.fetch(
+        'backgroundFetch',
+        [this.backgroundFetchUrl],
+        { title: 'Background Fetch' }
+      );
+
+      backgroundFetch.addEventListener('backgroundfetchsuccess', (event: any) => {
+        const fetchRecords = event.fetches;
+        fetchRecords.forEach((fetchRecord: any) => {
+          // Handle the fetch success and process the fetched data here
+          console.log('Background Fetch Success:', fetchRecord);
+        });
+      });
+
+      backgroundFetch.addEventListener('backgroundfetchfail', (event: any) => {
+        const fetchRecords = event.fetches;
+        fetchRecords.forEach((fetchRecord: any) => {
+          // Handle the fetch failure here
+          console.error('Background Fetch Failed:', fetchRecord);
+        });
+      });
+    } catch (error) {
+      console.error('Background Fetch Error:', error);
+    }
   }
 }
