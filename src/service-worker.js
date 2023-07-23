8dd7f8d1691e65e8f8ad6ba92cd5c9f14f4fa002
jai shukla <shuklajay012@gmail.com>
2023-07-23 16:36:10 +0530

    hotfix

--- src/service-worker.js
+++ src/service-worker.js
@@ -31,26 +31,6 @@ console.log(self,'SELF #####');
   //   }
   // // });
 
-  navigator.serviceWorker.ready.then(async (swReg) => {
-    const bgFetch = await swReg.backgroundFetch.fetch(
-      "my-fetch",
-      ["https://onlinetestcase.com/wp-content/uploads/2023/06/1.5-MB.pdf",
-       "https://media.istockphoto.com/id/1461268110/photo/growth-business-graph-finance-data-diagram-concept-on-stock-market-background-with-financial.jpg?s=2048x2048&w=is&k=20&c=5H6A3Ex4Ql7nXTcgUo8kaT3gnbukVxZrq6Wsr2REN3s="],
-      {
-        title: "Episode 5: Interesting things.",
-        icons: [
-          {
-            sizes: "300x300",
-            src: "https://media.istockphoto.com/id/1393890859/photo/happy-couple-at-home-paying-bills-online.jpg?s=2048x2048&w=is&k=20&c=joHosgeQiJnTNo-qBZOpUCPCWG3RpDqiG0E__Z7APC0=",
-            type: "image/png",
-          },
-        ],
-        downloadTotal: 60 * 1024 * 1024,
-      },
-    );
-
-    console.log(bgFetch,'BACKGROUND FETCH ########');
-  });
 
 
   if ('serviceWorker' in navigator) {
